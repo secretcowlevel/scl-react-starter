4509d4631260f2889322e18f3b58a60db36fcd59
Justin Reynard <framerate@gmail.com>
2015-12-28 14:37:45 -0600

    Upgrade to Babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,11 @@ module.exports = {
     },
     module: {
         loaders: [{
-            test: /\.js$/,
+            test: /.(js|jsx)?$/,
             loader: 'babel-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.jsx$/,
-            loaders: ['react-hot', 'babel'],
+            query: {
+                presets: ['es2015', 'react']
+            },
             exclude: /node_modules/
         }]
     },
